d1fbd22e678ae2ff9a0fe4b4a4d6a25825fe23de
Michael Klier <chi@chimeric.de>
2006-02-21 21:19:43 +0100

    removed blog-plugin-stuff :-/
    darcs-hash:20060221201943-ffd4a-94e75fb3ad47381cc5708adf0e5b470b0729072f.gz

--- main.php
+++ main.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 /**
  * DokuWiki Arctic Template
@@ -17,10 +19,7 @@
  */
 
  require_once(dirname(__FILE__).'/tpl_functions.php');
- include(DOKU_PLUGIN.'blog/functions.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>"
  lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction']?>">
 <head>
@@ -89,7 +88,6 @@
       </div>
       <div class="right_page">
         <?php tpl_content()?>
-        <?php html_discussion()?>
       </div>
 
     <?php } else { ?>
