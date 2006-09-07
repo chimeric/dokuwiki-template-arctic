60ce71a57aad1f41f5aa8f7cbc8b8c3d7e19aaf2
Michael Klier <chi@chimeric.de>
2006-09-07 15:40:23 +0200

    synced detail.php
    darcs-hash:20060907134023-484ab-4d1118d8abd8de394ec3a2cd6a3c36f7e892afed.gz

--- detail.php
+++ detail.php
@@ -15,11 +15,11 @@
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
 <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>
      <?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG))?>
-    [<?php echo hsc($conf['title'])?>]
+    [<?php echo strip_tags($conf['title'])?>]
   </title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
   <?php tpl_metaheaders()?>
 
@@ -27,65 +27,61 @@
 </head>
 
 <body>
-<div id="wrapper">
-    <div class="dokuwiki">
-      <?php html_msgarea()?>
+<div class="dokuwiki">
+  <?php html_msgarea()?>
 
-      <div class="page">
-          <?php if($ERROR){ print $ERROR; }else{ ?>
+  <div class="page">
+    <?php if($ERROR){ print $ERROR; }else{ ?>
 
-          <h1><?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG))?></h1>
+    <h1><?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG))?></h1>
 
-          <div class="img_big">
-            <?php tpl_img(900,700) ?>
-          </div>
-
-            <div class="img_detail">
-              <p class="img_caption">
-                  <?php print nl2br(hsc(tpl_img_getTag(array('IPTC.Caption',
-                                                   'EXIF.UserComment',
-                                                   'EXIF.TIFFImageDescription',
-                                                   'EXIF.TIFFUserComment')))); ?>
-            </p>
-
-          <p>&larr; <?php echo $lang['img_backto']?> <?php tpl_pagelink($ID)?></p>
-
-          <dl class="img_tags">
-            <?php
-                        $t = tpl_img_getTag('Date.EarliestTime');
-              if($t) print '<dt>'.$lang['img_date'].':</dt><dd>'.date($conf['dformat'],$t).'</dd>';
-
-              $t = tpl_img_getTag('File.Name');
-              if($t) print '<dt>'.$lang['img_fname'].':</dt><dd>'.hsc($t).'</dd>';
-
-              $t = tpl_img_getTag(array('Iptc.Byline','Exif.TIFFArtist','Exif.Artist','Iptc.Credit'));
-              if($t) print '<dt>'.$lang['img_artist'].':</dt><dd>'.hsc($t).'</dd>';
-
-              $t = tpl_img_getTag(array('Iptc.CopyrightNotice','Exif.TIFFCopyright','Exif.Copyright'));
-              if($t) print '<dt>'.$lang['img_copyr'].':</dt><dd>'.hsc($t).'</dd>';
-
-              $t = tpl_img_getTag('File.Format');
-              if($t) print '<dt>'.$lang['img_format'].':</dt><dd>'.hsc($t).'</dd>';
-
-              $t = tpl_img_getTag('File.NiceSize');
-              if($t) print '<dt>'.$lang['img_fsize'].':</dt><dd>'.hsc($t).'</dd>';
-
-              $t = tpl_img_getTag('Simple.Camera');
-              if($t) print '<dt>'.$lang['img_camera'].':</dt><dd>'.hsc($t).'</dd>';
-
-              $t = tpl_img_getTag(array('IPTC.Keywords','IPTC.Category'));
-              if($t) print '<dt>'.$lang['img_keywords'].':</dt><dd>'.hsc($t).'</dd>';
-
-            ?>
-          </dl>
-          <?php //Comment in for Debug// dbg(tpl_img_getTag('Simple.Raw'));?>
-            </div>
-        
-         <div class="clearer"></div>
-
-      <?php } ?>
-      </div>
+    <div class="img_big">
+      <?php tpl_img(900,700) ?>
     </div>
+
+    <div class="img_detail">
+      <p class="img_caption">
+        <?php print nl2br(hsc(tpl_img_getTag(array('IPTC.Caption',
+                                               'EXIF.UserComment',
+                                               'EXIF.TIFFImageDescription',
+                                               'EXIF.TIFFUserComment')))); ?>
+      </p>
+
+      <p>&larr; <?php echo $lang['img_backto']?> <?php tpl_pagelink($ID)?></p>
+
+      <dl class="img_tags">
+        <?php
+          $t = tpl_img_getTag('Date.EarliestTime');
+          if($t) print '<dt>'.$lang['img_date'].':</dt><dd>'.date($conf['dformat'],$t).'</dd>';
+
+          $t = tpl_img_getTag('File.Name');
+          if($t) print '<dt>'.$lang['img_fname'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag(array('Iptc.Byline','Exif.TIFFArtist','Exif.Artist','Iptc.Credit'));
+          if($t) print '<dt>'.$lang['img_artist'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag(array('Iptc.CopyrightNotice','Exif.TIFFCopyright','Exif.Copyright'));
+          if($t) print '<dt>'.$lang['img_copyr'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag('File.Format');
+          if($t) print '<dt>'.$lang['img_format'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag('File.NiceSize');
+          if($t) print '<dt>'.$lang['img_fsize'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag('Simple.Camera');
+          if($t) print '<dt>'.$lang['img_camera'].':</dt><dd>'.hsc($t).'</dd>';
+
+          $t = tpl_img_getTag(array('IPTC.Keywords','IPTC.Category'));
+          if($t) print '<dt>'.$lang['img_keywords'].':</dt><dd>'.hsc($t).'</dd>';
+
+        ?>
+      </dl>
+      <?php //Comment in for Debug// dbg(tpl_img_getTag('Simple.Raw'));?>
+    </div>
+
+  <?php } ?>
+  </div>
 </div>
 </body>
 </html>
