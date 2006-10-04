e327fa616f2c65778d5bceee0925a12c40ec0274
Michael Klier <chi@chimeric.de>
2006-10-04 22:44:10 +0200

    fixed config translation strings
    darcs-hash:20061004204410-484ab-32c72800c69fc6b92ffa8278e32c84902aede1d6.gz

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -10,10 +10,10 @@ $lang['pagename']                 = "pagename to use for sidebars";
 $lang['breadcrumbs']              = "breadcrumbs setup";
 $lang['wiki_actionlinks']         = "actionlink style";
 $lang['sepchar']                  = "actionlink separator";
-$lang['user_sidebar']             = "enable/disable per user-sidebars";
-$lang['user_sidebar_namespace']   = "namespace for users-sidebars";
-$lang['group_sidebar']            = "enable/disable per group-sidebars";
-$lang['group_sidebar_namespace']  = "namespace for group-sidebars";
-$lang['namespace_sidebar']        = "enable/disable namespace-sidebars";
+$lang['user_sidebar']             = "enable/disable per user sidebars";
+$lang['user_sidebar_namespace']   = "namespace for user sidebars";
+$lang['group_sidebar']            = "enable/disable per group sidebars";
+$lang['group_sidebar_namespace']  = "namespace for group sidebars";
+$lang['namespace_sidebar']        = "enable/disable namespace sidebars";
 $lang['sidebar_order']            = "sidebar order (M=main, U=user, G=group, N=namespace)";
 ?>
