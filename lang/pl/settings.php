008a559cc220b8406d0ddf2d9469f352a46c3a1f
Michael Klier <chi@chimeric.de>
2006-12-18 21:52:47 +0100

    added polish language file
    darcs-hash:20061218205247-23886-664f035569016ff991763269d93819c13754dfdb.gz

---
+++ lang/pl/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * polish language file for the configuration manager
+ */
+ 
+$lang['sidebar']                  = "położenie bocznego panelu (menu)";
+$lang['pagename']                 = "nazwa strony dla bocznego panelu";
+$conf['toc2sidebar']              = "pokaż TOC (spis treści strony) wewnątrz bocznego panelu"
+$lang['breadcrumbs']              = "ustawienie położenia śladu";
+$lang['wiki_actionlinks']         = "styl actionlink (linku)";
+$lang['user_sidebar']             = "włącz/wyłącz boczny panel użytkownika";
+$lang['user_sidebar_namespace']   = "nazwa katalogu dla paneli użytkowników";
+$lang['group_sidebar']            = "włącz/wyłącz boczny panel grupy";
+$lang['group_sidebar_namespace']  = "nazwa katalogu dla paneli grup";
+$lang['namespace_sidebar']        = "włącz/wyłącz panel katalogów";
+$lang['sidebar_order']            = "kolejność paneli [M=główne menu T=toc(spis strony) U=użytkownik G=grupa N=menu katalogu]";
+?>
