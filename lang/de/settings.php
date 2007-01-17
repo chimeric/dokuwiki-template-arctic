d2a10fcbda7b1a5c451f8e0b79be2410b3317091
Michael Klier <chi@chimeric.de>
2007-01-17 21:23:24 +0100

    added german language file
    darcs-hash:20070117202324-23886-15cab3587d2005ef377ac77d1acc6be2390ba19b.gz

---
+++ lang/de/settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * German Language file for the Arctic Template
+ * 
+ * @author:     Michael Klier <chi@chimeric.de>
+ */
+
+$lang['sidebar']                        = 'Sidebar Einstellungen';
+$lang['pagename']                       = 'Seitenname der Sidebars';
+$lang['trace']                          = '"Krümelpfad" Einstellungen';
+$lang['wiki_actionlinks']               = 'Actionlink Darstellung';
+$lang['user_sidebar_namespace']         = 'Namensraum für Benutzer Sidebars';
+$lang['group_sidebar_namespace']        = 'Namesraum für Gruppen Sidebars';
+$lang['left_sidebar_order']             = 'Reihenfolge der linken Sidebars (Komma getrennt)';
+$lang['left_sidebar_content']           = 'Inhalt der linken Sidebar';
+$lang['left_sidebar_other']             = 'Andere Sidebars (Komma getrennt)';
+$lang['right_sidebar_order']            = 'Reihenfolge der rechten Sidebars (Komma getrennt)';
+$lang['right_sidebar_content']          = 'Inhalt der rechten Sidebar';
+$lang['left_sidebar_other']             = 'Andere Sidebars (Komma getrennt)';
+$lang['search']                         = 'Position des Suchfelds (wenn 2 Sidebars benutzt werden)';
+?>
