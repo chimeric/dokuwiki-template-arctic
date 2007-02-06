c80356cba5516dd7156115344e0466979a60234d
Michael Klier <chi@chimeric.de>
2007-02-06 22:50:17 +0100

    updated polish translation
    darcs-hash:20070206215017-23886-7b864a972a2a962551fd49e265078cc11adb684b.gz

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -1,14 +1,19 @@
 <?php
 /**
- * polish language file for the configuration manager
- * 
- *
+ * polish language file for the Arctic Template
  */
- 
-$lang['sidebar']                  = "położenie bocznego panelu (menu)";
-$lang['pagename']                 = "nazwa strony dla bocznego panelu";
-$lang['trace']                    = "ustawienie położenia śladu";
-$lang['wiki_actionlinks']         = "styl actionlink (linku)";
-$lang['user_sidebar_namespace']   = "nazwa katalogu dla paneli użytkowników";
-$lang['group_sidebar_namespace']  = "nazwa katalogu dla paneli grup";
+
+$lang['sidebar']                        = "położenie menu (bocznego panelu)";
+$lang['pagename']                       = "nazwa strony dla menu";
+$lang['trace']                          = "ustawienie położenia śladu";
+$lang['wiki_actionlinks']               = "style linków funkcyjnych (actionlink)";
+$lang['user_sidebar_namespace']         = "nazwa katalogu dla menu użytkowników";
+$lang['group_sidebar_namespace']        = "nazwa katalogu dla menu grup";
+$lang['left_sidebar_order']             = "kolejność menu (rozdzielone przecinkami)";
+$lang['left_sidebar_content']           = "zawartość lewego menu";
+$lang['left_sidebar_content_other']     = "inne menu (rozdzielone przecinkami)";
+$lang['right_sidebar_order']            = "kolejność menu (rozdzielone przecinkami)";
+$lang['right_sidebar_content']          = "zawartość prawego menu";
+$lang['right_sidebar_content_other']    = "inne menu (rozdzielone przecinkami)";
+$lang['search']                         = "pozycja pola wyszukiwania (ma zastosowanie tylko gdy używane są 2 menu)";
 ?>
