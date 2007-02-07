79c5de7044996a3c077106832b51e0fad2fac8f0
Michael Klier <chi@chimeric.de>
2007-02-07 21:51:29 +0100

    refactored english translation
    darcs-hash:20070207205129-23886-14d1f79d2e33c12e9ebd6a2ddd26e6a7bf13b576.gz

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,17 +5,17 @@
  * @author:     Michael Klier <chi@chimeric.de>
  */
 
-$lang['sidebar']                        = "sidebar setup";
-$lang['pagename']                       = "pagename to use for sidebars";
-$lang['trace']                          = "trace setup";
-$lang['wiki_actionlinks']               = "actionlink style";
-$lang['user_sidebar_namespace']         = "namespace for user sidebars";
-$lang['group_sidebar_namespace']        = "namespace for group sidebars";
-$lang['left_sidebar_order']             = "left sidebar order (comma separated)";
-$lang['left_sidebar_content']           = "content of the left sidebar";
-$lang['left_sidebar_content_other']     = "other sidebars (comma separated)";
-$lang['right_sidebar_order']            = "right sidebar order (comma separated)";
-$lang['right_sidebar_content']          = "content of the right sidebar";
-$lang['right_sidebar_content_other']    = "other sidebars (comma separated)";
-$lang['search']                         = "position of the search field (applies only if 2 sidebars are used)";
+$lang['sidebar']                        = "Sidebar setup";
+$lang['pagename']                       = "Pagename to use for sidebars";
+$lang['trace']                          = "Tace setup";
+$lang['wiki_actionlinks']               = "Actionlink style";
+$lang['user_sidebar_namespace']         = "Namespace for user sidebars";
+$lang['group_sidebar_namespace']        = "Namespace for group sidebars";
+$lang['left_sidebar_order']             = "Left sidebar order (comma separated)";
+$lang['left_sidebar_content']           = "Content of the left sidebar";
+$lang['left_sidebar_content_other']     = "Other sidebars (comma separated)";
+$lang['right_sidebar_order']            = "Right sidebar order (comma separated)";
+$lang['right_sidebar_content']          = "Content of the right sidebar";
+$lang['right_sidebar_content_other']    = "Other sidebars (comma separated)";
+$lang['search']                         = "Position of the search field (applies only if 2 sidebars are used)";
 ?>
