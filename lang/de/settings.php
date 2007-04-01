c8d959adcb0c5555673bc449ecf0a13e8f1c79bc
Michael Klier <chi@chimeric.de>
2007-04-01 14:49:58 +0200

    fixed german language file
    darcs-hash:20070401124958-23886-a76bef1169a40f382764531aa0c39c53c945836e.gz

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -13,9 +13,9 @@ $lang['user_sidebar_namespace']         = 'Namensraum für Benutzer Sidebars';
 $lang['group_sidebar_namespace']        = 'Namesraum für Gruppen Sidebars';
 $lang['left_sidebar_order']             = 'Reihenfolge der linken Sidebars (Komma getrennt)';
 $lang['left_sidebar_content']           = 'Inhalt der linken Sidebar';
-$lang['left_sidebar_other']             = 'Andere Sidebars (Komma getrennt)';
+$lang['left_content_sidebar_other']     = 'Andere Sidebars (Komma getrennt)';
 $lang['right_sidebar_order']            = 'Reihenfolge der rechten Sidebars (Komma getrennt)';
 $lang['right_sidebar_content']          = 'Inhalt der rechten Sidebar';
-$lang['left_sidebar_other']             = 'Andere Sidebars (Komma getrennt)';
+$lang['left_content_sidebar_other']     = 'Andere Sidebars (Komma getrennt)';
 $lang['search']                         = 'Position des Suchfelds (wenn 2 Sidebars benutzt werden)';
 ?>
