b3d457ec3859bca2f89e745d7c40a67856e4b1e4
Michael Klier <chi@chimeric.de>
2009-08-22 15:12:22 +0200

    removed index from default sidebar settings

--- conf/default.php
+++ conf/default.php
@@ -13,10 +13,10 @@ $conf['group_sidebar_namespace']    = 'group';                      // namespace
 $conf['trace']                      = 1;                            // show trace at top of the page
 $conf['main_sidebar_always']		    = 1;														// show main sidebar on all namespaces
 $conf['wiki_actionlinks']           = 'links';                      // use buttons instead of links
-$conf['left_sidebar_content']       = 'index,main,user,group,namespace';  // defines the content of the left sidebar
-$conf['left_sidebar_order']         = 'index,main,namespace,user,group';  // defines the order of the left sidebar content
-$conf['right_sidebar_content']      = 'index,main,user,group,namespace';  // defines the content of the right sidebar
-$conf['right_sidebar_order']        = 'index,main,namespace,user,group';  // defines the order of the right sidebar content
+$conf['left_sidebar_content']       = 'main,user,group,namespace';  // defines the content of the left sidebar
+$conf['left_sidebar_order']         = 'main,namespace,user,group';  // defines the order of the left sidebar content
+$conf['right_sidebar_content']      = 'main,user,group,namespace';  // defines the content of the right sidebar
+$conf['right_sidebar_order']        = 'main,namespace,user,group';  // defines the order of the right sidebar content
 $conf['search']                     = 'left';                       // defines the position  of the search form when 2 sidebars are used
 $conf['closedwiki']                 = 0;                            // don't show sidebars for logged out users at all
 
